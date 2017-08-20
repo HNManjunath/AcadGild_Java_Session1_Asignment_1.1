6b8f2875c546123548ed652c2cca86169d7ca250
MANJUNATH <MANJUNATH@SAMSUNG.Manjunath>
2017-08-20 22:51:30 +0530

    Committing the helloworld program

---
+++ src/com/corejava/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.corejava.helloworld;
+
+/**
+ * @author MANJUNATH HN
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// To print Hello world.
+		System.out.println("Hello World..!!");
+	}
+
+}
